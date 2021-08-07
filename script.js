d4568b2abd9ec8632162ae25c45c4ee267b84242
SergeyKurshin <mr.kurshin@mail.ru>
2021-08-08 01:07:30 +0800

    Edit button Share

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let button = document.querySelector('.share-button');
-button.addEventListener('click', function(){
-    let test = getComputedStyle(button).backgroundColor;
-    let socialActive = document.querySelector('.icon-social');
-    let buttonShare = document.querySelector('.share-button > svg > path');
-    if(test === "rgb(236, 242, 248)") {
-        button.style.backgroundColor = "rgb(109,127,151)";
-        socialActive.classList.add("icon-social--active");
-        buttonShare.style.fill = "white";
-    } else {
-        button.style.backgroundColor = "rgb(236, 242, 248)";
-        socialActive.classList.remove("icon-social--active");
-        buttonShare.style.fill = "rgb(110,128,152)";
-    }
-})
+const button = document.querySelector('.share-button');
+
+button.addEventListener('click', function () {
+  let socialNone = document.querySelector('.icon-social-none');
+  let socialActive = document.querySelector('.icon-social-active');
+    !socialActive ?
+      socialNone.classList.add("icon-social-active") :
+      socialNone.classList.remove("icon-social-active");
+  console.log("test", event.target)
+}, true);
+
+window.onclick = function () {
+  let socialNone = document.querySelector('.icon-social-none');
+  if (event.target !== button) socialNone.classList.remove("icon-social-active");
+}
